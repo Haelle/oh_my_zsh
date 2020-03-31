644a030a25d1d1d6a9ee8d11762eb743b7a95f33
Alexis Leclerc <alexis@alxs.fr>
2020-03-31 12:09:35 +0300

    Add JWT functions

---
+++ files/jwt.zsh
@@ -0,0 +1,29 @@
+# Decode jwt in console !
+decode_base64_url() {
+  local len=$((${#1} % 4))
+  local result="$1"
+  if [ $len -eq 2 ]; then result="$1"'=='
+  elif [ $len -eq 3 ]; then result="$1"'='
+  fi
+  echo "$result" | tr '_-' '/+' | openssl enc -d -base64
+}
+
+decode_jwt(){
+   decode_base64_url $(echo -n $2 | cut -d "." -f $1) | jq .
+}
+
+full_decode_jwt() {
+  echo '=Header=='
+  decode_jwt 1 $1
+  echo '==Payload=='
+  decode_jwt 2 $1
+}
+
+# Decode JWT
+alias jwt="full_decode_jwt"
+
+# Decode JWT header
+alias jwth="decode_jwt 1"
+
+# Decode JWT Payload
+alias jwtp="decode_jwt 2"
